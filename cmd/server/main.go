48730ac3739a317fdae8a5b43646e5ec80554289
Lucas Pope <lpopedv@proton.me>
2024-06-16 09:44:58 -0300

    feat: implement app endpoints

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
+	"github.com/lpopedv/user-products-api/configs"
+	"github.com/lpopedv/user-products-api/infra/database"
+	"github.com/lpopedv/user-products-api/infra/webserver/handlers"
+	"github.com/lpopedv/user-products-api/internal/entity"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	configs, err := configs.LoadConfig(".")
+
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("sqlite.db"), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB, configs.TokenAuth, configs.JWTExpiresIn)
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/{id}", productHandler.FindById)
+		r.Get("/", productHandler.GetProducts)
+		r.Put("/{id}", productHandler.Update)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Post("/users", userHandler.Create)
+
+	r.Post("/sessions", userHandler.Sessions)
+
+	http.ListenAndServe(":8000", r)
 }
